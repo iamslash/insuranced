7452d3262362c115593b38a5f1c92b6032f70880
iamslash <iamslash@gmail.com>
2016-09-30 12:12:44 +0900

    configured eslintrc

--- src/routes/input.js
+++ src/routes/input.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-     res.sendFile(path.join(__dirname + '/../../docs/input.html'));
+  res.sendFile(path.join(__dirname + '/../../docs/input.html'));
 });
 
 module.exports = router;

7c327595fd724e21aaf4108d4a8f1abd88f86680
iamslash <iamslash@gmail.com>
2017-01-17 22:30:38 +0900

    angularjs added

---
+++ angularjs/quickstart-angularjs/app/scripts/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('quickStartApp',['ui.select'])
+	.controller('MainCtrl', ['$scope', function ($scope) {
+
+		$scope.slogan = {
+			text : 'Hello, Quick start AngularJS'
+		};
+
+		$scope.words = [{
+			text : 'Hello, Quick start AngularJS'
+		},{
+			text : 'Hello'
+		},{
+			text : 'Quickstart'
+		},{
+			text : 'AngularJS'
+		},{
+			text : 'WebFramework'
+		}];
+
+		$scope.tip = {
+			text : "Yes It's me"
+		};
+
+
+
+	}]);

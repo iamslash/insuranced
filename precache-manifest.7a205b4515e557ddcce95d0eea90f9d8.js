318d5e04c6710f56d9df53755a693b221bd19fbb
David Sun <iamslash@gmail.com>
2020-03-02 12:29:58 +0900

    Updates

---
+++ precache-manifest.7a205b4515e557ddcce95d0eea90f9d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1b9e5f4281403180b4d7ef74454c7d",
+    "url": "/examplesofweb/index.html"
+  },
+  {
+    "revision": "464d4ac60dc0fbde3212",
+    "url": "/examplesofweb/static/css/main.24d18a4a.chunk.css"
+  },
+  {
+    "revision": "9a53c27654cdc9b45052",
+    "url": "/examplesofweb/static/js/2.7c35c3a7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/examplesofweb/static/js/2.7c35c3a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464d4ac60dc0fbde3212",
+    "url": "/examplesofweb/static/js/main.439911cf.chunk.js"
+  },
+  {
+    "revision": "c54fd53bbd705a9c3af3",
+    "url": "/examplesofweb/static/js/runtime-main.b3b5424d.js"
+  }
+]);

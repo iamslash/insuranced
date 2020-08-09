f0137ac872a477e0aff88a633e863831a9f2e7d3
David Sun <iamslash@gmail.com>
2020-08-09 17:44:25 +0900

    Practicing this setState

--- snippets/react/my-movie/src/App.js
+++ snippets/react/my-movie/src/App.js
@@ -23,14 +23,39 @@ const movies = [
 ]
 
 class App extends Component {
+
   state = {
-    greeting: 'Hello'
-  }
+    greeting: 'Hello World',
+    movies: [
+      {
+        title: "Matrix",
+        poster: 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2014/1202/IE001778581_STD.jpg',
+      },
+      {
+        title: "Full Metal Jacket",
+        poster: 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2014/1202/IE001778470_STD.jpg',
+      },
+      {
+        title: "Oldboy",
+        poster: 'https://cdn1.thr.com/sites/default/files/imagecache/768x433/2017/06/143289-1496932680-mm_2012_047_italy_57_-_h_2017.jpg',
+      },
+      {
+        title: "Star wars",
+        poster: 'https://cdn1.thr.com/sites/default/files/2017/06/143226-1496932903-mm_2012_047_italy_11_-_embed_2018.jpg',
+      },
+    ]    
+  }  
   componentDidMount() {
     setTimeout(() => {
       //this.state.greeting = 'something'
       this.setState({
-        greeting: 'Hello Again'
+        movies: [
+        ...this.state.movies,
+        {
+          tilte: "Trainspotting",
+          poster: 'https://cdn1.thr.com/sites/default/files/2017/06/143226-1496932903-mm_2012_047_italy_11_-_embed_2018.jpg',
+        }
+      ]
       })
     }, 2000)
   }
@@ -39,7 +64,7 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>

c822239549b2ea678195536b213d4fa54e1f149b
David Sun <iamslash@gmail.com>
2020-08-09 17:45:55 +0900

    Loading states

--- snippets/react/my-movie/src/App.js
+++ snippets/react/my-movie/src/App.js
@@ -23,50 +23,46 @@ const movies = [
 ]
 
 class App extends Component {
-
   state = {
     greeting: 'Hello World',
-    movies: [
-      {
-        title: "Matrix",
-        poster: 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2014/1202/IE001778581_STD.jpg',
-      },
-      {
-        title: "Full Metal Jacket",
-        poster: 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2014/1202/IE001778470_STD.jpg',
-      },
-      {
-        title: "Oldboy",
-        poster: 'https://cdn1.thr.com/sites/default/files/imagecache/768x433/2017/06/143289-1496932680-mm_2012_047_italy_57_-_h_2017.jpg',
-      },
-      {
-        title: "Star wars",
-        poster: 'https://cdn1.thr.com/sites/default/files/2017/06/143226-1496932903-mm_2012_047_italy_11_-_embed_2018.jpg',
-      },
-    ]    
   }  
   componentDidMount() {
     setTimeout(() => {
+      console.log("change state...")
       //this.state.greeting = 'something'
       this.setState({
         movies: [
-        ...this.state.movies,
-        {
-          tilte: "Trainspotting",
-          poster: 'https://cdn1.thr.com/sites/default/files/2017/06/143226-1496932903-mm_2012_047_italy_11_-_embed_2018.jpg',
-        }
-      ]
+          {
+            title: "Matrix",
+            poster: 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2014/1202/IE001778581_STD.jpg',
+          },
+          {
+            title: "Full Metal Jacket",
+            poster: 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2014/1202/IE001778470_STD.jpg',
+          },
+          {
+            title: "Oldboy",
+            poster: 'https://cdn1.thr.com/sites/default/files/imagecache/768x433/2017/06/143289-1496932680-mm_2012_047_italy_57_-_h_2017.jpg',
+          },
+          {
+            title: "Star wars",
+            poster: 'https://cdn1.thr.com/sites/default/files/2017/06/143226-1496932903-mm_2012_047_italy_11_-_embed_2018.jpg',
+          },
+        ]    
       })
     }, 2000)
+  }
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies;
   }
   render() {
     console.log("render");
     return (
       <div className="App">
-        {this.state.greeting}
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading...'}
       </div>
     );
   }

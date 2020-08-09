6f48b1beba844107a6c785d6c954ff55c7bf75be
David Sun <iamslash@gmail.com>
2020-08-09 17:34:05 +0900

    Data flow with Props

--- snippets/react/my-movie/src/App.js
+++ snippets/react/my-movie/src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <Movie />
-      <Movie />
-    </div>
-  );
+const movieTitles = [
+  "Matrix",
+  "Full Metal Jacket",
+  "Oldboy",
+  "Star wars"
+]
+
+const movieImages = [
+  'http://ojsfile.ohmynews.com/STD_IMG_FILE/2014/1202/IE001778581_STD.jpg',
+  'http://ojsfile.ohmynews.com/STD_IMG_FILE/2014/1202/IE001778470_STD.jpg',
+  'https://cdn1.thr.com/sites/default/files/imagecache/768x433/2017/06/143289-1496932680-mm_2012_047_italy_57_-_h_2017.jpg',
+  'https://cdn1.thr.com/sites/default/files/2017/06/143226-1496932903-mm_2012_047_italy_11_-_embed_2018.jpg',
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
+        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
+        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
+        <Movie title={movieTitles[3]} poster={movieImages[3]}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- snippets/react/my-movie/src/Movie.js
+++ snippets/react/my-movie/src/Movie.js
@@ -3,10 +3,11 @@ import './Movie.css';
 
 class Movie extends Component {
   render() {
+    console.log(this.props);
     return (
       <div>
-        <MoviePoster />
-        <h1>Hello this is a movie</h1>
+        <MoviePoster poster={this.props.poster}/>
+        <h1>{this.props.title}</h1>
       </div>
     )
   }
@@ -15,7 +16,7 @@ class Movie extends Component {
 class MoviePoster extends Component {
   render() {
     return (
-      <img src='http://ojsfile.ohmynews.com/STD_IMG_FILE/2014/1202/IE001778581_STD.jpg'/>
+      <img src={this.props.poster}/>
     )
   }
 }

57f9424c781fc54b710d3ba950e03f6376609792
David Sun <iamslash@gmail.com>
2020-08-09 17:56:50 +0900

    Async Await

--- snippets/react/my-movie/src/App.js
+++ snippets/react/my-movie/src/App.js
@@ -3,60 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2014/1202/IE001778581_STD.jpg',
-  },
-  {
-    title: "Full Metal Jacket",
-    poster: 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2014/1202/IE001778470_STD.jpg',
-  },
-  {
-    title: "Oldboy",
-    poster: 'https://cdn1.thr.com/sites/default/files/imagecache/768x433/2017/06/143289-1496932680-mm_2012_047_italy_57_-_h_2017.jpg',
-  },
-  {
-    title: "Star wars",
-    poster: 'https://cdn1.thr.com/sites/default/files/2017/06/143226-1496932903-mm_2012_047_italy_11_-_embed_2018.jpg',
-  },
-]
-
 class App extends Component {
-  state = {
-    greeting: 'Hello World',
-  }  
+  state = {};
   componentDidMount() {
-    setTimeout(() => {
-      console.log("change state...")
-      //this.state.greeting = 'something'
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster: 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2014/1202/IE001778581_STD.jpg',
-          },
-          {
-            title: "Full Metal Jacket",
-            poster: 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2014/1202/IE001778470_STD.jpg',
-          },
-          {
-            title: "Oldboy",
-            poster: 'https://cdn1.thr.com/sites/default/files/imagecache/768x433/2017/06/143289-1496932680-mm_2012_047_italy_57_-_h_2017.jpg',
-          },
-          {
-            title: "Star wars",
-            poster: 'https://cdn1.thr.com/sites/default/files/2017/06/143226-1496932903-mm_2012_047_italy_11_-_embed_2018.jpg',
-          },
-        ]    
-      })
-    }, 2000)
+    this._getMovies();
   }
   _renderMovies = () => {
     const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={index} />
     })
     return movies;
+  }
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+  }
+  _callApi = () => {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = 'https://yts.ag/api/v2/list_movies.json?sort_by=rating'; // site that doesn’t send Access-Control-*
+    return fetch(proxyurl + url)
+    .then((resp) => resp.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err));
   }
   render() {
     console.log("render");
